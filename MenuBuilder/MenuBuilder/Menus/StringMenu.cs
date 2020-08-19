7e8f5905213440791c1269c56a302bf7494b2126
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-19 17:33:57 +0300

    change menu Actions from Action to Func<string>

--- MenuBuilder/MenuBuilder/Menus/StringMenu.cs
+++ MenuBuilder/MenuBuilder/Menus/StringMenu.cs
@@ -8,18 +8,18 @@ namespace MenuBuilder.Menus
     public class StringMenu:IMenu
     {
         private StringBuilder _content;
-        public Dictionary<string, Action> Actions { get; set; }
+        public Dictionary<string, Func<string>> Actions { get; set; }
 
 
         public StringMenu()
         {
             _content = new StringBuilder();
-            Actions = new Dictionary<string, Action>();
+            Actions = new Dictionary<string, Func<string>>();
         }
 
         public string Content { get => _content.ToString(); }
 
-        public void AddAction(string key, Action action, string description)
+        public void AddAction(string key, Func<string> action, string description)
         {
             Actions.Add(key, action);
             _content.AppendLine($"{key} : for {description}");
